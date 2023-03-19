acbbbc3de77771fbcba2ee8eb91957f7c6381ce2
Sanjid Bari <sanjidbari@Sanjids-MacBook-Air.local>
2023-03-19 13:42:18 -0400

    done

--- script/script.js
+++ script/script.js
@@ -25,6 +25,27 @@ document.getElementById('rectangle-button').addEventListener('click', function()
     const area = width * length;
     if(isNaN(area) == true || area <= 0){  
         alert('Please type positive numbers only');
+        document.getElementById('length').value = '';
+        document.getElementById('width').value = '';
+    }
+    else {
+        const display = document.getElementById('calculation-area');
+        const div = document.createElement('div');
+        div.classList.add("my-4");
+        div.innerHTML = `<p class="inline text-xl">2. Rectangle ${area} cm<sup>2</sup></p>
+        <button class="p-2 bg-blue-500 text-xl text-white rounded-md ml-2">meter</button>`;
+        display.appendChild(div);
+    }
+})
+
+// parallelogram area
+
+document.getElementById('parallelogram-button').addEventListener('click', function(){
+    const height = document.getElementById('height').value;
+    const base = document.getElementById('base').value;
+    const area = base * height;
+    if(isNaN(area) == true || area <= 0){  
+        alert('Please type positive numbers only');
         document.getElementById('base').value = '';
         document.getElementById('height').value = '';
     }
@@ -32,7 +53,72 @@ document.getElementById('rectangle-button').addEventListener('click', function()
         const display = document.getElementById('calculation-area');
         const div = document.createElement('div');
         div.classList.add("my-4");
-        div.innerHTML = `<p class="inline text-xl">2. Rectangle ${area} cm<sup>2</sup></p>
+        div.innerHTML = `<p class="inline text-xl">3. Parallelogram ${area} cm<sup>2</sup></p>
+        <button class="p-2 bg-blue-500 text-xl text-white rounded-md ml-2">meter</button>`;
+        display.appendChild(div);
+    }
+})
+
+// Rhombus area
+
+document.getElementById('rhombus-button').addEventListener('click', function(){
+    const d1 = document.getElementById('d1').value;
+    const d2 = document.getElementById('d2').value;
+    const area = 0.5 * d1 * d2;
+    if(isNaN(area) == true || area <= 0){  
+        alert('Please type positive numbers only');
+        document.getElementById('d1').value = '';
+        document.getElementById('d2').value = '';
+    }
+    else {
+        const display = document.getElementById('calculation-area');
+        const div = document.createElement('div');
+        div.classList.add("my-4");
+        div.innerHTML = `<p class="inline text-xl">4. Rhombus ${area} cm<sup>2</sup></p>
+        <button class="p-2 bg-blue-500 text-xl text-white rounded-md ml-2">meter</button>`;
+        display.appendChild(div);
+    }
+})
+
+// pentagon area
+
+document.getElementById('pentagon-button').addEventListener('click', function(){
+    const p = document.getElementById('p').value;
+    const b = document.getElementById('b').value;
+    const area = 0.5 * p * b;
+    if(isNaN(area) == true || area <= 0){  
+        alert('Please type positive numbers only');
+        document.getElementById('p').value = '';
+        document.getElementById('b').value = '';
+    }
+    else {
+        const display = document.getElementById('calculation-area');
+        const div = document.createElement('div');
+        div.classList.add("my-4");
+        div.innerHTML = `<p class="inline text-xl">5. Pentagon ${area} cm<sup>2</sup></p>
+        <button class="p-2 bg-blue-500 text-xl text-white rounded-md ml-2">meter</button>`;
+        display.appendChild(div);
+    }
+})
+
+// ellipse area
+
+document.getElementById('ellipse-button').addEventListener('click', function(){
+    const a = document.getElementById('a').value;
+    const b = document.getElementById('b-ellipse').value;;
+    const area = 3.14 * a * b;
+    area.toFixed(2);
+    if(isNaN(area) == true || area <= 0){
+        console.log(area);  
+        alert('Please type positive numbers only');
+        document.getElementById('a').value = '';
+        document.getElementById('b-ellipse').value = '';
+    }
+    else {
+        const display = document.getElementById('calculation-area');
+        const div = document.createElement('div');
+        div.classList.add("my-4");
+        div.innerHTML = `<p class="inline text-xl">6. Ellipse ${area} cm<sup>2</sup></p>
         <button class="p-2 bg-blue-500 text-xl text-white rounded-md ml-2">meter</button>`;
         display.appendChild(div);
     }
